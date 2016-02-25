8ad044a8263cd13d53bc82c7c502293c629cea62
Roger Andersen <hexjelly@hexjelly.com>
2016-02-25 12:50:07 +0100

    Mock up battle info tab

--- background.js
+++ background.js
@@ -1,7 +1,22 @@
 
+// battle notifier functions
 function setTimeIndicator (time) {
+  if (time === false) {
+    chrome.browserAction.setBadgeText(null);
+  }
   chrome.browserAction.setBadgeBackgroundColor({color:[106, 161, 33, 255]});
   chrome.browserAction.setBadgeText({text: "\u00A0"});
 }
 
-setTimeIndicator();
+
+// misc functions
+function startBackground () {
+  setTimeIndicator(10);
+}
+
+function stopBackground () {
+  setTimeIndicator(false);
+}
+
+// start background processing
+startBackground();
--- options.js
+++ options.js
@@ -5,7 +5,6 @@ function save_options() {
     favoriteColor: color,
     likesColor: likesColor
   }, function() {
-    // Update status to let user know options were saved.
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
     setTimeout(function() {
@@ -14,8 +13,6 @@ function save_options() {
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
@@ -26,6 +23,7 @@ function restore_options() {
     document.getElementById('like').checked = items.likesColor;
   });
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
