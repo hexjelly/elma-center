b91c980a7c7c8c9aa7423a26ca975527b5cd40c8
Roger Andersen <hexjelly@hexjelly.com>
2016-02-26 17:38:35 +0100

    Add battle attributes bitmasks

--- background.js
+++ background.js
@@ -1,6 +1,28 @@
+/* jshint esversion: 6 */
 
-// battle notifier functions
-function setTimeIndicator (time) {
+// battle API
+const FLAGS = [
+  { "mask": 1, "attr": "See others" },
+  { "mask": 2, "attr": "See times" },
+  { "mask": 4, "attr": "Allow starter" },
+  { "mask": 8, "attr": "Apple bug" },
+  { "mask": 16, "attr": "No volt" },
+  { "mask": 32, "attr": "No turn" },
+  { "mask": 64, "attr": "One turn" },
+  { "mask": 128, "attr": "No brake" },
+  { "mask": 256, "attr": "No throttle" },
+  { "mask": 512, "attr": "Always throttle" },
+  { "mask": 1024, "attr": "Drunk" },
+  { "mask": 4096, "attr": "One wheel" },
+  { "mask": 8192, "attr": "Multi" }
+];
+
+const TYPES = [
+  "Normal", "One life", "First finish", "Slowness", "Survivor", "Last result", "Finish count", "1 Hour TT", "Flag tag", "Apple", "Speed"
+];
+
+// sets extension icon badge color based on time left of battle
+function setTimeIndicatorIcon (time) {
   if (time === false) {
     chrome.browserAction.setBadgeText(null);
   }
@@ -8,17 +30,20 @@ function setTimeIndicator (time) {
   chrome.browserAction.setBadgeText({text: "\u00A0"});
 }
 
+// TODO: remember to only fetch level info once for each lev hurr durr!
+function getBattleInfo () {
 
-// misc functions
+}
+
+// start watching for new battles
 function startBackground () {
   setTimeIndicator(10);
 }
 
+// stop watching for new battles
 function stopBackground () {
   setTimeIndicator(false);
 }
-
-// TODO: remember to only fetch level info once for each lev hurr durr!
 
 // start background processing
 startBackground();
